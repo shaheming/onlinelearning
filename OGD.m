df84817cc19a58ee8887b4bf0b23e38f8c045503
shaheming <hemingsha@gmail.com>
2017-07-09 18:01:06 -0700

    Fix the regert bug

--- OGD.m
+++ OGD.m
@@ -1,8 +1,11 @@
+% Set turns
+T = 5000
 % G is  positive retional number begin with  1
 G = (rand(100,1) + 0.1)' ;
 % your decision domain used in projection
 Xb = 0;
 Xe = 100;
+Ex_n = 1000;
 % D and eta are used in reward function U
 D = 1;
 eta = 1;
@@ -10,17 +13,23 @@ eta = 1;
 outx = [];
 outy = [];
 outr = [];
+myRewards = [];
+experts =Xe * rand(Ex_n,1);
+expertsRewards = zeros(1,Ex_n);
 
 % the initial y
 y = 50.5;
 disp('Begin Loop');
-for i = 0 : 999
+for i = 0 : T 
     Z = D * rand(99,1);
     x_t = project(y,Xb,Xe);
     y = y + (1 / (i+1))*((Ut_z(x_t,Z,G,eta)));
     outx = [outx,x_t];
-    outy = [outy,Ut(x_t,Z,G,eta) - Ut(30,Z,G,eta)];
-    outr = [outr,sum(outr)+outy(i+1)];
+    tmp_xt = zeros(1,Ex_n);
+    myRewards = [myRewards,Ut(x_t,Z,G,eta)];
+    expertsRewards =expertsRewards + Ut(experts,Z,G,eta);
+%     outy = [outy,myRewards(i+1) - min(Ut(experts,Z,G,eta))];
+    outr = [outr,sum(myRewards) - max(expertsRewards)];
 end
 disp('End Loop');
 
@@ -28,8 +37,8 @@ figure('name','The value of Xt','NumberTitle','off','Position',[0,500,700,500]);
 plot(outx);
 figure('name','The aluve of regret','NumberTitle','off','Position',[700,500,700,500]);
 plot(outr);
-figure('name','The distance between my prediction and expert prediction in every turn','NumberTitle','off','Position',[700,1000,700,500]);
-plot(outy);
+% figure('name','The distance between my prediction and expert prediction in every turn','NumberTitle','off','Position',[700,1000,700,500]);
+% plot(outy);
 
 % the difference of reward function U
 function [u] = Ut_z(x_t,Z,G,eta)
@@ -39,7 +48,7 @@ end
 % the reward function U
 function [u] = Ut(x_t,Z,G,eta)
 gz = G(2:end) * Z;
-u = -0.5 * (G(1)*x_t - gz - eta)^2;
+u = -0.5 * (G(1).*x_t - gz - eta).^2;
 end
 % the projection funciton
 function x_t = project(y_t,Xb,Xe)
