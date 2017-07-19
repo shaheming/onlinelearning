4d09bed8b5c3e99e2e903df89a7586101bbffaa2
shaheming <hemingsha@gmail.com>
2017-07-19 08:37:11 -0700

    Add smd

--- singleAgent/SMD/SMD.m
+++ singleAgent/SMD/SMD.m
@@ -1,62 +1,29 @@
-function out = SMD()
+function out = SMD(M)
 %use doubling tricking to iterate
-M = 10; % 2 ^ 15 = 32768
+% M = 18; % 2 ^ 15 = 32768
 % the maxiums turn will iterate T times;
 T = 2^(M)-1; % avoid the last value to 0
 % T = 50000;
 
-global r_bound;
-r_bound = [0,5];
+global x_bound;
+x_bound = [0,1;0,2*pi];
+y0 = [2,8/6*pi];
 % D and eta are used in reward function U
-global D;
-D = 2*pi;
-
-y0 = 2;
-
 global eta;
 eta = 1;
 
-global thetas;
-thetas  = zeros(1,T+1); % <G , Z>
-% output variable
-global regrets;
-regrets = zeros(1,T);
-
-global experts;
-experts = zeros(1,T);
-global expertsRewards;
-expertsRewards = zeros(1,T);
 global myChoices;
-myChoices = zeros(1,T);
-global myRewards;
-myRewards = zeros(1,T);
+myChoices = zeros(T,2);
+
 
 %%%%%%%%%%%%%%%%%%
 % main function  %
 %%%%%%%%%%%%%%%%%%
-  isDraw =true;
-%   rng(1);
-%   out_d=OGD_doubling(M,y1,isDraw);
-%   
-%   figure('name','Regrets Compare','NumberTitle','off','Position',[100,0,700,500]);
-%   plot(out_d,'DisplayName','doubling');
-%   hold on;
-  
-   rng(1);
-   out = OGD_Primary(T,y0,isDraw);
-%    plot(out,'DisplayName','omd');
-%    legend('doubling','omd');
-%    
-%    hold off;
-%   
-%   rng(1);
-%   regret_s=ogdfix(8,r_bound);
-%   figure('name','RG','NumberTitle','off','Position',[0,500,700,500]);
-%   plot(out,'DisplayName','out_s');
-%   hold on;
-%   plot(regret_s,'DisplayName','regret_s');
-%   legend('out_s','regret_s');
-%   hold off;
+  isDraw =false;
+
+%    rng(2);
+    OGD_Primary(T,y0,isDraw);
+
 %%%%%%%end%%%%%%%%
 
 
@@ -64,51 +31,53 @@ end
 
 
 
-function out = OGD_Primary(T,y0,isDraw)
-  global experts;
-  global myChoices;
+function  OGD_Primary(T,y0,isDraw)
 
-%   output variable
-%   regrets = zeros(1,T);
-%   regrets_div_t = zeros(1,T);
-%   experts = zeros(1,T);
-%   expertsRewards = zeros(1,T);
-%   myChoices = zeros(1,T);
-%   myRewards = zeros(1,T);
+  global myChoices;
+  mkdir img;
    
   disp('Begin Loop');
   fprintf('Iterate %d turns',T);
   
-   [~,regretsOut ]= iteration(1,T,y0,false);
+    iteration(1,T,y0,false);
   
-  disp('End Loop');
-  
-  if isDraw
-    figure('name','The value of Xt','NumberTitle','off','Position',[0,500,700,500]);
-    plot(experts,'DisplayName','experts');
-    hold on;
-    plot(myChoices,'DisplayName','mychoice');
-    legend('experts','mychoice');
-    hold off;
-    figure('name','The aluve of regret','NumberTitle','off','Position',[700,500,700,500]);
-    plot(regretsOut);
-%     figure('name','Regret div t','NumberTitle','off','Position',[700,0,700,500]);
-%     plot(regrets_div_t);
-  end
-  out = regretsOut;
+
+   disp('End Loop'); 
+   step = 100;
+
+   r =0:1/step:1;
+   theta = 0:2*pi/step:2*pi;
+   [THETA,R]=meshgrid(theta,r);
+   G = (3 + sin(5*THETA)+ cos(3*THETA)).*(R.^2).*(5/3-R);
+   
+   regFig = figure('name','SGD','NumberTitle','off');
+   hold on;
+   mesh(R.*cos(THETA), R.*sin(THETA), G);
+   hold on;
+   shading interp
+   Z=zeros(1,T);
+   for i = 1:T
+     Z(i) = userLoss(myChoices(i,:));
+   end
+   
+   Z = ones(1,T)*4;
+
+   r=myChoices(:,1);
+   theta = myChoices(:,2);
+   plot3(r.*cos(theta),r.*sin(theta),Z,'Color','r');
+   hold off;
+   
+   xFigName = sprintf('%s-%s-r0=%.3f-theta=%.3fend','SGD','X',y0(1),y0(2));
+   saveas(regFig,strcat('img/',xFigName),'png');
+
 end
 
 
-function [rOut,regretsOut ]=iteration(t_b,t_e,y0,doubling_flag)
+function iteration(t_b,t_e,y0,doubling_flag)
 
-  global r_bound;
-  global D;
-  global thetas;
-  global regrets;
-  global experts;
-  global expertsRewards;
+  global x_bound;
   global myChoices;
-  global myRewards;
+
   
   %theta
 
@@ -116,17 +85,10 @@ function [rOut,regretsOut ]=iteration(t_b,t_e,y0,doubling_flag)
   % start at 0 OMG this is a serious problem !!! because in matlab for i =
   % i = 1:1 will iterate
   if t_b == 1
-    % I think there is problem
-    % If we choose iterate from 1
-    % The X_1 is decided by which parameter
-    % This simulation is a little bit different from the normal algorithm
-    % in the paper. 
-    % In my code we get x at first, then we gerate y
-    % get x0
-    r = project(y0,r_bound);
+
+    x_0 = project(y0,x_bound);
     %x0 feedback
-    thetas(1) = rand(1) * D;    
-    y = y0 - 1*gradient(r,thetas(1));
+    y = y0 - 1*gradient(x_0);
   end
    
   for t = t_b : t_e
@@ -137,40 +99,17 @@ function [rOut,regretsOut ]=iteration(t_b,t_e,y0,doubling_flag)
     if doubling_flag 
       eta1 = t_b+1; 
     else
-      eta1 = t + 1;
+      eta1 = t + 10;
     end
     
     % get x_t
-    myChoices(t) = project(y,r_bound);
-    thetas(t) = rand(1) * D;
-    % get theta_t + 1
-    y = y - (1 / eta1)*gradient(myChoices(t),thetas(t));
-    
-    % my rewards
-    if t == 1
-       myRewards(1)  = userLoss(myChoices(1),thetas(1));
-    else
-       myRewards(t)  = myRewards(t-1) + userLoss(myChoices(t),thetas(t));
-    end
-    
-    % caculate expert choice
+    myChoices(t,:) = project(y,x_bound);
 
-    u = updateExpert(experts,t,thetas);
-    u = project(u,r_bound);
-    
-    % record expert's choice
-    experts(t) = u;
-    %expert's rewards
-    
-    expertsRewards(t) = expertLoss(experts(t),thetas,t);
-   
-    %regret
-    regrets(t) = myRewards(t) - expertsRewards(t);
-    
+    y = y - (1 / eta1)*gradient(myChoices(t,:));
+        
   end
   
-  rOut = r;
-  regretsOut = regrets;
+
 end
 
 function u = updateExpert(experts,t,thetas)
@@ -178,12 +117,19 @@ function u = updateExpert(experts,t,thetas)
   u = 0;
 end
 % the difference of reward function U
-function uout = gradient(r,theta)
-  uout = (3+ sin(5*theta) + cos(3*theta))*(10/3 * r - 3 * r^2);
+function outX = gradient(x)
+  r = x(1);
+  theta = x(2);
+  
+
+  outX(1) = (3+ sin(5*theta) + cos(3*theta))*(10/3 * r - 3 * r^2);
+  outX(2) =(5*cos(5*theta) - 3*sin(3*theta))*r^2*(5/3 - r);
 end
 
 % the reward function U
-function uout = userLoss(r,theta)
+function uout = userLoss(x)
+  theta = x(2);
+  r = x(1);
   uout = (3+ sin(5*theta) + cos(3*theta))* r^2 *(5/3-r);
 end
 
@@ -191,14 +137,16 @@ function uout = expertLoss(r,thetas,t)
   uout = 0;
 end
 %the projection funciton
-function x_t = project(y_t,r_bound)
-  if r_bound(1) <= y_t && y_t <= r_bound(2)
-    x_t = y_t;
-  else
-    if y_t < r_bound(1)
-      x_t = r_bound(1);
+function x_t = project(y_t,x_bound)
+  for i = 1:2
+    if x_bound(i,1) <= y_t(i) && y_t(i) <=x_bound(i,2)
+      x_t(i) = y_t(i);
     else
-      x_t = r_bound(2);
+      if y_t(i) < x_bound(i,1)
+         x_t(i) = x_bound(i,1);
+      else
+         x_t(i) = x_bound(i,2);
+      end
     end
   end
 end
